d4ad1dbd32b233ea56f1d7e9163c69f804ee977c
Christener19 <christenerleigh7070@gmail.com>
2023-11-10 15:28:14 +0000

    form working but submiting () even the the inputs are empty

--- src/App.js
+++ src/App.js
@@ -6,16 +6,21 @@ import Gallery from "./Gallery";
 import data from "./gallery.json";
 import "./App.css";
 
-console.log(data);
-
 function App() {
   const [artworkData, setArtworkData] = useState(data);
+
+  // Function to add new artwork
+  const addArtwork = (newArtwork) => {
+    // Adding an id to the new artwork
+    const newArtworkWithId = { ...newArtwork, id: artworkData.length + 1 };
+    setArtworkData([...artworkData, newArtworkWithId]);
+  };
 
   return (
     <>
       <Header />
       <Gallery artworkData={artworkData} />
-      <Form />
+      <Form onAddArtwork={addArtwork} /> {/* Pass the function as a prop */}
     </>
   );
 }
--- src/Form.js
+++ src/Form.js
@@ -1,17 +1,37 @@
-export default function Form() {
+import { useState } from 'react';
+
+export default function Form({ onAddArtwork }) {
+    const [picture, setPicture] = useState('');
+    const [title, setTitle] = useState('');
+    const [artist, setArtist] = useState('');
+    const [year, setYear] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); // Prevents page refresh
+        const newArtwork = { picture, title, artist, year }; // Create new artwork object
+        onAddArtwork(newArtwork); // Call the function passed from parent to add the artwork
+        // Reset form fields
+        setPicture('');
+        setTitle('');
+        setArtist('');
+        setYear('');
+    };
+
     return (
-        <form className="form-footer">
-
-      <label htmlFor="picture">Picture: </label>
-      <input type="text" id="picture" name="picture" placeholder="e.g. 'http://imgur.com/GHJ59'"/>
-      <label htmlFor="title">Title: </label>
-      <input type="text" id="title" name="title" placeholder="e.g. 'Mona Lisa'"/>
-      <label htmlFor="artist">Artist: </label>
-      <input type="text" id="artist" name="artist" placeholder="e.g. 'Leonardo da Vinci'"/>
-      <label htmlFor="year">Year: </label>
-      <input type="text" id="year" name="year" placeholder="e.g. '1503'"/>
-      <button className="add">Add</button>
-
+        <form className="form-footer" onSubmit={handleSubmit}>
+            <label htmlFor="picture">Picture: </label>
+            <input type="text" id="picture" value={picture} onChange={(e) => setPicture(e.target.value)} placeholder="e.g. 'http://imgur.com/GHJ59'"/>
+            
+            <label htmlFor="title">Title: </label>
+            <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="e.g. 'Mona Lisa'"/>
+            
+            <label htmlFor="artist">Artist: </label>
+            <input type="text" id="artist" value={artist} onChange={(e) => setArtist(e.target.value)} placeholder="e.g. 'Leonardo da Vinci'"/>
+            
+            <label htmlFor="year">Year: </label>
+            <input type="text" id="year" value={year} onChange={(e) => setYear(e.target.value)} placeholder="e.g. '1503'"/>
+            
+            <button className="add" type="submit">Add</button>
         </form>
-    )
+    );
 }
