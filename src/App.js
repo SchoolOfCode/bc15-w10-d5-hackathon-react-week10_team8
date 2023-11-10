6f8dbc1d5bce1640e6f42aeebef70b99dcebd749
Danny Ryan <dannykryan@gmail.com>
2023-11-10 12:33:58 +0000

    render data to artcard

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import Header from './Header';
+import { useState } from "react";
+
+import Header from "./Header";
 // import Form from './Form';
-import Gallery from './Gallery';
-import data from './gallery.json';
-import './App.css';
+import Gallery from "./Gallery";
+import data from "./gallery.json";
+import "./App.css";
 
 function App() {
+  const [artworkData, setArtworkData] = useState(data);
+
   return (
-   <>
-    <Header />
-    {/* <Form/> */}
-    <Gallery />
-   </>
+    <>
+    <p>This is the main App</p>
+      <Header />
+      <Gallery artworkData={artworkData} />
+    </>
   );
 }
 
--- src/Gallery.js
+++ src/Gallery.js
@@ -1,10 +1,9 @@
 // import data from './gallery.json';
 import Artcard from "./Artcard";
 
-export default function Gallery() {
-    return (
-        <main>
-            <Artcard artwork={artwork} />
-        </main>
-    )
+export default function Gallery(props) {
+  console.log(props.artworkData);
+  return (<main>
+    <h3>{props.artworkData[0].title}</h3>
+  </main>);
 }
